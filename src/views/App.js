cf98315151a9bb33db7a84102a8137f53406d0c7
gungorn <gungorn@outlook.com>
2020-07-16 21:49:18 +0300

    fb test

--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import database from '@react-native-firebase/database';
+const db = database();
+
 class App extends React.Component {
+
+    componentDidMount() {
+        this.ekle('/NOTLAR/KULLANICIID1', 'test');
+        this.ekle('/NOTLAR/KULLANICIID2', 'test');
+        this.ekle('/NOTLAR/KULLANICIID3', { test: { test: { test: { a: 'a' } } } });
+        this.ekle('/NOTLAR/KULLANICIID4', 'test');
+        this.ekle('/', 'hahaha');
+    }
+
+
+    ekle(yol, veri) {
+        const ref = db.ref(yol);
+
+        const islem = ref.set(veri);
+
+        islem.then(d => console.log({ sonuc: true, data: d }))
+            .catch(e => console.log({ sonuc: true, error: e }));
+
+    }
+
     render() {
         return (
             <View>
