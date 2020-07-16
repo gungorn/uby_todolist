8862273e7624b445a06f4cd7734f86b338f6b8da
gungorn <gungorn@outlook.com>
2020-07-16 19:53:37 +0300

    first commit : create project

---
+++ index.js
@@ -0,0 +1,10 @@
+import { AppRegistry, UIManager, Platform } from 'react-native';
+import App from './src//views/App';
+
+
+if (Platform.OS === 'android') UIManager.setLayoutAnimationEnabledExperimental(true);
+
+//console.disableYellowBox = true;
+
+
+AppRegistry.registerComponent('todolist', () => App);
