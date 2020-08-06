4941bb2a299556eea6b4bc33990ee8639489c90c
gungorn <gungorn@outlook.com>
2020-08-06 20:13:49 +0300

    console.count fonksiyonları için paket import edildi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { AppRegistry, UIManager } from 'react-native';
 import App from './src//views/App';
 import tlfnH from './src/helper/tlfnH';
+import 'react-native-console-time-polyfill';
 
 
 if (tlfnH.android) UIManager.setLayoutAnimationEnabledExperimental(true);
