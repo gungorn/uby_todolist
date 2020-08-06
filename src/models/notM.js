6e206d6910531cda8f506cc41fc892107cc4adbd
gungorn <gungorn@outlook.com>
2020-08-06 20:12:43 +0300

    notlar sunucudan çekliyor

--- src/models/notM.js
+++ src/models/notM.js
@@ -1,17 +1,59 @@
 import { observable, action, decorate } from 'mobx';
-import fbH from '../helper/fbH';
 
-import anasayfaC from '../controllers/anasayfa/anasayfaC'
+import fbH from '../helper/fbH';
+import uyelikM from './uyelikM';
 
 class notM {
-    ekleNot = async () => {
-        const { baslik, notIcerik } = anasayfaC;
-        const notVeri = { baslik, notIcerik };
+    notlar = []; //OTURUM AÇAN KULLANICIYA AİT NOTLAR
 
+    ekleNot = async (baslik, notIcerik, notid) => {
         try {
-            const sonuc = await fbH.ekleNot(notVeri);
+            const sonuc = await fbH.ekleNot({ baslik, notIcerik }, notid);
+            return { sonuc: true };
+        }
+        catch (e) { return { sonuc: false, hata: e }; }
+    }
+
+    eslestirKullaniciNot = async (uid, notid) => {
+        try {
+            const sonuc = await fbH.eslestirKullaniciNot(uid, notid);
 
             return { sonuc: true };
+        }
+        catch (e) { return { sonuc: false, hata: e }; }
+    }
+
+
+    getirNotlar = async () => {
+        try {
+            const notidArray = Object.values(await this.getirKullaniciNotid(uyelikM.uid));
+            const notlar = [];
+
+            for (let i = 0; i < notidArray.length; i++) {
+                const notid = notidArray[i].notid;
+                const not = await this.getirNot(notid);
+                notlar.push({ notid, ...not });
+            }
+
+            this.notlar = notlar;
+
+            return notlar;
+        }
+        catch (e) { }
+    }
+    getirKullaniciNotid = async (uid) => {
+        try {
+            const sonuc = await fbH.getirKullaniciNotid(uid);
+
+            return sonuc;
+        }
+        catch (e) { return { sonuc: false, hata: e }; }
+    }
+    getirNot = async (notid) => {
+        try {
+            const sonuc = await fbH.getirNot(notid);
+
+            return sonuc;
         }
         catch (e) { return { sonuc: false, hata: e }; }
     }
@@ -20,7 +62,16 @@ class notM {
 decorate(
     notM,
     {
+        notlar: observable,
+
+
         ekleNot: action,
+
+        eslestirKullaniciNot: action,
+
+        getirNotlar: action,
+        getirKullaniciNotid: action,
+        getirNot: action
     }
 );
 
